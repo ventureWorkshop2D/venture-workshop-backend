8cc0612687b38c26ea1e4aaa8c1e196a68d5258f
siwoo <k74415834@gmail.com>
2024-08-27 14:01:30 +0900

    DOCS: swagger 적용

---
+++ src/main/java/com/project/homeListener/common/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.project.homeListener.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private Info apiInfo() {
+        return new Info()
+                .title("home listener api docs") // API의 제목
+                .description("API docs") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
