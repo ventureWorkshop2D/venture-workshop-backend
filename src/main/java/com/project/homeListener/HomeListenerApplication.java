8e8b1f977dc33f267574588e77bf255f004a2e87
siwoo <k74415834@gmail.com>
2024-08-27 12:59:26 +0900

    INIT: init project

---
+++ src/main/java/com/project/homeListener/HomeListenerApplication.java
@@ -0,0 +1,13 @@
+package com.project.homeListener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/homeListener/HomeListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.homeListener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
